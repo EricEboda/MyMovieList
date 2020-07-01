ce010caa6965e74a3ea8ab71c923d2412051db3d
CodeEryk <ericjeb3@gmail.com>
2020-07-01 12:48:29 +0100

    JQuery for JS added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 $(':radio').change(function() {
     console.log('New star rating: ' + this.value);
   });
+
+// START OF MOVIES JQUERY
+
+$(document).ready(() => {
+  
+});
+
+// END OF MOVIES JQUERY
